c94f8b791335982275c2ea5cb58ee66df6c1bcc4
shobhit9070 <shobhitrock80@gmail.com>
2021-06-08 16:37:06 +0530

    my app

--- django/trydjango/trydjango/urls.py
+++ django/trydjango/trydjango/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
-
+from pages.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
+    path('',home_view,name='home'),
     path('admin/', admin.site.urls),
     path('idea/', include('idea.url')),
-]
+    path('about/',about_view),
+    path('contact/',contact_view) 
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += staticfiles_urlpatterns()
