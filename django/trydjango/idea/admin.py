0cad8a2aceb0c352cebcb28d65a50d79694b3d08
shubham69kumar <shubham69kumar0903@gmail.com>
2021-06-13 15:24:57 +0530

    changes

--- django/trydjango/idea/admin.py
+++ django/trydjango/idea/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import question
+from .models import Question
 
-admin.site.register(question)
+admin.site.register(Question)
--- django/trydjango/idea/url.py
+++ django/trydjango/idea/url.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'idea'
 urlpatterns = [
     path('', views.what_to_start, name='first'),
+
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
